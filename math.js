33a35a0dd55daa68d632a13013b698e30cdd4b74
hashaaan <shalithadev@gmail.com>
2025-03-02 22:12:48 +0530

    02/03/2025 - BT03 - S04

---
+++ math.js
@@ -0,0 +1,12 @@
+const user = {
+  name: "Hashan",
+  age: 29,
+};
+
+// named object export
+export { user };
+
+// named function export
+export function add(a, b) {
+  return a + b;
+}
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// import { add } from "./math";
+// import { add } from "./math.js";
 // console.log("I'm second javascript file here!", userEmail);
 
 // Primitive Types
@@ -134,5 +134,87 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 // console.log("I'm here after DOM Load!");
 
-console.log("Username is : ", localStorage.getItem("username"));
-console.log("Email is : ", sessionStorage.getItem("email"));
+// console.log("Username is : ", localStorage.getItem("username"));
+// console.log("Email is : ", sessionStorage.getItem("email"));
+
+// console.log("Result of 100 + 50 :", add(100, 50));
+
+// if (sessionStorage.getItem("name") === null) {
+//   console.log("Name is not set!");
+//   // sessionStorage.setItem("name", "Hashan");
+// } else {
+//   console.log("Name is :", sessionStorage.getItem("name"));
+// }
+
+// let myPromise = new Promise((resolve, reject) => {
+//   let success = true;
+//   if (success) {
+//     // Simulate a delay of 3 seconds
+//     setTimeout(() => resolve("Operation is successful!"), 3000);
+//   } else {
+//     reject("Operation is failed!");
+//   }
+// });
+
+// // myPromise
+// //   .then((message) => {
+// //     console.log(message); // Operation is successful!
+// //   })
+// //   .catch((error) => {
+// //     console.log(error); // Operation is failed!
+// //   })
+// //   .finally(() => {
+// //     console.log("Promise is completed!");
+// //   });
+
+// // Async & Await
+// async function initializeApp() {
+//   try {
+//     const message = await myPromise;
+//     console.log("Message : ", message);
+
+//     const userResponse = await fetch(
+//       "https://jsonplaceholder.typicode.com/users/1"
+//     );
+//     const user = await userResponse.json();
+
+//     // Check if the user object is not null
+//     if (user?.id) {
+//       console.log("(1) User : ", user);
+//       const postsResponse = await fetch(
+//         `https://jsonplaceholder.typicode.com/posts?userId=${user.id}`
+//       );
+//       const posts = await postsResponse.json();
+//       console.log("(2) Posts : ", posts);
+//     }
+//   } catch (error) {
+//     console.log("Error fetching data : ", error);
+//   }
+// }
+
+// // Call the async function
+// initializeApp();
+
+// console.log("I'm here after the async function!");
+
+// Define a function that accepts a callback
+function fetchData(callback) {
+  console.log("Fetching Started!");
+
+  setTimeout(() => {
+    let data = { name: "Hashan", age: 29 };
+    callback(data);
+    console.log("Timeout Completed!");
+  }, 3000); // Simulate network request with a delay of 3 seconds
+
+  console.log("Fetching Completed!");
+}
+
+// Define a callback function
+function handleData(data) {
+  // console.log("Data received : ", data);
+}
+
+// Call the function with the callback
+// Pass a argument to the fetchData function
+fetchData(handleData);
